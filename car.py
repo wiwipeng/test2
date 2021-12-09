856ffb96e0ec6341b3d13f10ada9fe22c53131b0
wiwipeng <95857611+wiwipeng@users.noreply.github.com>
2021-12-09 20:53:33 +0800

    Update car.py

--- car.py
+++ car.py
@@ -5,7 +5,6 @@ speed= st.number_input('車速的資料收集:限速100 =>')
 temp = st.number_input('溫度的資料收集:限溫30 =>')
 tur=int(input('轉速資料的收集:限轉12000 =>'))
 if cofirm_input:
-      st.write('第六組')
    if gas <= car_simulation.get('gas_warning'):
       st.write('油箱只剩', gas, '格! 準備加油!!')
    else:
