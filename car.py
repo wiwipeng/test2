40c8be7dbac3219c742fb7d95539c83dfcdb3bfd
wiwipeng <95857611+wiwipeng@users.noreply.github.com>
2021-12-09 21:14:18 +0800

    Update car.py

--- car.py
+++ car.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.titel('第六組')
 car_simulation = {'gas_warning':1, 'speed_limit':100, 'temp_warning':30, 'turn':12000}
 gas = st.number_input('油量的資料收集:油箱滿是10格 =>')
 speed= st.number_input('車速的資料收集:限速100 =>')
